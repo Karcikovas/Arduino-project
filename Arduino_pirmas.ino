66b494d520010d7ab89f2d2aaccd8b6e8ff1b2f6
Viktoras Karcikovas <45361674+Karcikovas@users.noreply.github.com>
2019-05-14 13:07:59 +0300

    Its a first Arduino project

---
+++ Arduino_pirmas.ino
@@ -0,0 +1,21 @@
+#define trigPin 7
+#define echoPin 6
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+void loop(){
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  duration= pulseIn(echoPin, HIGH);
+  distance = (3.4 * duration)*200;
+  Serial.println(duration);
+  delay(500);
+}
+
